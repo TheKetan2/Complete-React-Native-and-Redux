bb045ff86ba986bf17db23325b2ddf880750eb9d
Ketan <ketantoo@gmail.com>
2019-10-31 18:17:14 +0530

    new page navigation using props.navigation.navigate('Route Name')

--- StarterApp/src/screens/HomeScreen.js
+++ StarterApp/src/screens/HomeScreen.js
@@ -1,7 +1,19 @@
 import React from "react";
-import { Text, StyleSheet, View } from "react-native";
-const HomeScreen = () => {
-  return <Text style={styles.text}>Hello0</Text>;
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
+const HomeScreen = props => {
+  // console.log(props);
+  return (
+    <View>
+      <Text style={styles.text}>HomeScreen</Text>
+      <Button
+        title="Go to Component Demo"
+        onPress={() => props.navigation.navigate("Components")}
+      ></Button>
+      <TouchableOpacity onPress={() => props.navigation.navigate("List")}>
+        <Text>Go to List Demo</Text>
+      </TouchableOpacity>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
