efdc9c0a860702bbc4922894fcda9be364a515eb
Ketan <ketantoo@gmail.com>
2019-11-04 21:07:07 +0530

    styling search box done

--- foodApp/src/screens/component/SearchBar.js
+++ foodApp/src/screens/component/SearchBar.js
@@ -1,19 +1,32 @@
 import React from "react";
 import { View, Text, StyleSheet, TextInput } from "react-native";
+import { Feather } from "@expo/vector-icons";
 
 const SearchBar = () => {
   return (
-    <View style={styles.background}>
-      <Text>Search Bar</Text>
+    <View style={styles.backgroundStyle}>
+      <Feather name="search" style={styles.iconStyle}></Feather>
+      <TextInput placeholder="Search" style={styles.inputStyle}></TextInput>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  background: {
+  backgroundStyle: {
     backgroundColor: "#F0EEEE",
     height: 50,
     borderRadius: 5,
+    marginHorizontal: 15,
+    flexDirection: "row",
+    marginVertical: 15
+  },
+  inputStyle: {
+    flex: 9,
+    fontSize: 18
+  },
+  iconStyle: {
+    fontSize: 35,
+    alignSelf: "center",
     marginHorizontal: 15
   }
 });
