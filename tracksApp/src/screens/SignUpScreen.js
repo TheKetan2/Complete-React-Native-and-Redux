ac8b0cfdad085d6ba3594cf76de398896492c4ac
Ketan <ketantoo@gmail.com>
2019-11-15 14:56:18 +0530

    using react-native-elements to get better ui

--- tracksApp/src/screens/SignUpScreen.js
+++ tracksApp/src/screens/SignUpScreen.js
@@ -1,22 +1,38 @@
 import React from "react";
-import { View, StyleSheet, Text, Button } from "react-native";
+import { View, StyleSheet } from "react-native";
+import { Text, Input, Button, Header } from "react-native-elements";
+import Spacer from "../conponents/Spacer";
 
 const SignUpScreen = ({ navigation }) => {
   return (
-    <View>
-      <Text> Signup Screen</Text>
-      <Button
-        title="Go to sign in"
-        onPress={() => navigation.navigate("SignIn")}
-      />
-      <Button
-        title="Go to main flow"
-        onPress={() => navigation.navigate("mainFlow")}
-      />
+    <View style={styles.container}>
+      <Spacer>
+        <Text h3> Sign Up for Tracker</Text>
+      </Spacer>
+
+      <Input label="Email" />
+      <Spacer />
+      <Input label="Password" />
+      <Spacer />
+      <Spacer>
+        <Button title="Sign Up" />
+      </Spacer>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+SignUpScreen.navigationOptions = () => {
+  return {
+    header: null
+  };
+};
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: "center",
+    flex: 1,
+    marginBottom: 200
+  }
+});
 
 export default SignUpScreen;
